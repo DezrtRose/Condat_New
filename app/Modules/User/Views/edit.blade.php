175e3ec7597c34cbda54fe479309a7a9ab4d464d
Jenish <metheexplorer@outlook.com>
2016-09-26 13:55:36 +1000

    edit user master

--- app/Modules/User/Views/edit.blade.php
+++ app/Modules/User/Views/edit.blade.php
@@ -21,38 +21,25 @@
 
                 <ul class="list-group list-group-unbordered">
                     <li class="list-group-item">
-                        <b>Followers</b> <a class="pull-right">1,322</a>
-                    </li>
-                    <li class="list-group-item">
-                        <b>Following</b> <a class="pull-right">543</a>
-                    </li>
-                    <li class="list-group-item">
-                        <b>Friends</b> <a class="pull-right">13,287</a>
+                         <strong><i class="fa fa-calendar margin-r-5"></i> User Id</strong>
+
+                        <p class="text-muted">{{format_id($user->id, 'U')}}</p>
+
+                        <hr>
+                        <strong><i class="fa fa-calendar margin-r-5"></i> Member Since</strong>
+
+                        <p class="text-muted">{{shorten_date($user->created_at)}}</p>
+
+                    
                     </li>
                 </ul>
 
-                {{--<a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>--}}
             </div>
             <!-- /.box-body -->
         </div>
         <!-- /.box -->
 
-        <!-- About Me Box -->
-        <div class="box box-primary">
-            <div class="box-header with-border">
-                <h3 class="box-title">General Information</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-                <strong><i class="fa fa-calendar margin-r-5"></i> Member Since</strong>
-
-                <p class="text-muted">{{shorten_date($user->created_at)}}</p>
-
-                <hr>
-            </div>
-            <!-- /.box-body -->
-        </div>
-        <!-- /.box -->
+        
     </div>
     <div class="col-xs-9">
         <div class="box box-primary">
