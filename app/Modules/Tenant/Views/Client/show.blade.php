3604ce3d07a3b6e50451a2d14a770acd3a8a8d07
excursion <excursion@gnail.com>
2016-12-06 10:17:56 +0545

    layout fix

--- app/Modules/Tenant/Views/Client/show.blade.php
+++ app/Modules/Tenant/Views/Client/show.blade.php
@@ -6,11 +6,7 @@
     <li>View</li>
 @stop
 @section('content')
-
-    <div class="container">
-        <div class="row">
-            @include('Tenant::Client/client_header')
-        </div>
+        @include('Tenant::Client/client_header')
         @if (count($errors) > 0)
             <div class="alert alert-danger">
                 <ul>
@@ -22,71 +18,64 @@
         @endif
         @include('flash::message')
         <div class="col-md-3">
+            <!-- About Me Box -->
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">General Information</h3>
+                </div>
 
-            <div class="row">
-                <!-- About Me Box -->
-                <div class="box box-primary">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">General Information</h3>
-                    </div>
+                <!-- /.box-header -->
+                <div class="box-body">
 
-                    <!-- /.box-header -->
-                    <div class="box-body">
+                    <strong><i class="fa fa-file-text-o margin-r-5"></i> Client ID</strong>
 
-                        <strong><i class="fa fa-file-text-o margin-r-5"></i> Client ID</strong>
+                    <p class="text-muted">{{format_id($client->client_id, 'C')}}</p>
 
-                        <p class="text-muted">{{format_id($client->client_id, 'C')}}</p>
+                    <strong><i class="fa fa-calendar margin-r-5"></i> Created At</strong>
 
-                        <strong><i class="fa fa-calendar margin-r-5"></i> Created At</strong>
+                    <p class="text-muted">{{format_datetime($client->created_at)}}</p>
 
-                        <p class="text-muted">{{format_datetime($client->created_at)}}</p>
+                    <strong><i class="fa fa-user-plus margin-r-5"></i> Created By</strong>
 
-                        <strong><i class="fa fa-user-plus margin-r-5"></i> Created By</strong>
+                    <p class="text-muted">{{get_tenant_name($client->added_by)}}</p>
 
-                        <p class="text-muted">{{get_tenant_name($client->added_by)}}</p>
+                    <strong><i class="fa fa-file-text-o margin-r-5"></i> Due Amount</strong>
 
-                        <strong><i class="fa fa-file-text-o margin-r-5"></i> Due Amount</strong>
+                    <p class="text-muted">{{ format_price($due_payment) }}</p>
 
-                        <p class="text-muted">{{ format_price($due_payment) }}</p>
+                    <strong><i class="fa fa-file-text-o margin-r-5"></i> Referred By</strong>
 
-                        <strong><i class="fa fa-file-text-o margin-r-5"></i> Referred By</strong>
-
-                        <p class="text-muted">{{ $client->referred_by }}</p>
+                    <p class="text-muted">{{ $client->referred_by }}</p>
 
 
-                    </div>
                 </div>
             </div>
             <!-- /.box-body -->
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Reminders</h3>
 
-            <div class="row">
-                <div class="box box-primary">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Reminders</h3>
-
-                        <a href="{{url($tenant_id."/clients/$client->client_id/notes")}}"
-                           class="btn btn-success pull-right btn-sm"><i class="fa fa-plus"></i> Add New</a>
+                    <a href="{{url($tenant_id."/clients/$client->client_id/notes")}}"
+                       class="btn btn-success pull-right btn-sm"><i class="fa fa-plus"></i> Add New</a>
 
 
-                    </div>
-                    <!-- Recent Payments -->
-                    <div class="box-body">
-                        @if(count($remainders) > 0)
-                            @foreach($remainders as $key => $remainder)
-                                <strong><i class="fa fa-file-text-o margin-r-5"></i> {{ $remainder->description }}
-                                </strong>
+                </div>
+                <!-- Recent Payments -->
+                <div class="box-body">
+                    @if(count($remainders) > 0)
+                        @foreach($remainders as $key => $remainder)
+                            <strong><i class="fa fa-file-text-o margin-r-5"></i> {{ $remainder->description }}
+                            </strong>
 
-                                <p class="text-muted">{{ format_date($remainder->reminder_date) }}</p>
-                            @endforeach
-                        @else
-                            <p class="text-muted well">
-                                No upcoming reminders.
-                            </p>
-                        @endif
-                    </div>
+                            <p class="text-muted">{{ format_date($remainder->reminder_date) }}</p>
+                        @endforeach
+                    @else
+                        <p class="text-muted well">
+                            No upcoming reminders.
+                        </p>
+                    @endif
                 </div>
             </div>
-
             <!-- /.box -->
         </div>
 
@@ -98,7 +87,6 @@
                 <!-- /.post -->
             </div>
         </div>
-    </div>
     <!-- /.col -->
 
 
